eeb8404ca1276ef95b6affe8d9b509c9f70ab6f6
Vane Andonov <vane.andonov@gmail.com>
2013-02-22 09:21:48 +0100

    first commit readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Killerspree::Application.config.secret_token = '81afcd6b258fb8f765cbd7c9c96c29d7cd26e65ac3f7d04c8b50037c1916d68495209b9c2d650a6bdbf2f8e04b290b9186a885b3c3652f50192eb5530b9d24bb'
